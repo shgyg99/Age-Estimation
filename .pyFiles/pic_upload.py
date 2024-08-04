ad6d8bfedc8b7cbc7ae7921b0e8d9b8f4c3b7844
Shaqayeq Matlabi <108318987+shgyg99@users.noreply.github.com>
2024-08-04 08:55:31 +0330

    picture uploader function added

--- .pyFiles/pic_upload.py
+++ .pyFiles/pic_upload.py
@@ -1,32 +1,38 @@
-# def picture(image_path, transform, model, face_detect=False):
-#   if face_detect:
-#     detector = face_detection.build_detector(
-#           "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
-#     image = cv2.imread(image_path)
-#     im = Image.open(image_path)
-#     im2 = transform(im).permute(1, 2, 0)
-#     pic = plt.imshow(im)
-#     dets = detector.detect(
-#                         image[:, :, ::-1]
-#                     )[:, :4]
-#     for bbox in dets:
-#         top, right, bottom, left = [int(_) for _ in bbox]
-#         cv2.rectangle(image, (top, right), (bottom, left), (0, 0, 255), 2)
-#         img = TF.to_tensor(im)[top:bottom, left:right].permute(2, 0, 1).unsqueeze(0)
-#         img = test_transform(img)
-#         age = int(model(img).item())
-#         label = f'age:{age}'
-#         (w, h), _ = cv2.getTextSize(
-#             label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-#         cv2.putText(image, label, (bottom, right - 5),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+import torch
+import face_detection
+import cv2
+import matplotlib.pyplot as plt
+from torchvision.transforms import functional as TF
+from transforms import test_transform
+from functions import AgeEstimationModel
+
+AgeEstimationModel()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-#   else:
-#     img = Image.open(image_path).convert('RGB')
+def picture(model_path, image_path):    
+    model = torch.load(model_path, map_location=torch.device(device))
+    model.eval()
 
-#     img_tensor = transform(img).unsqueeze(0)
-#   with torch.inference_mode():
-#     preds = model(img_tensor.to(device)).item()
+    detector = face_detection.build_detector(
+            "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
+    image =cv2.imread(image_path)
+    dets = detector.detect(
+                            image[:, :, ::-1]
+                        )[:, :4]
+    for bbox in dets:
+        top, right, bottom, left = [int(_) for _ in bbox]
+        cv2.rectangle(image, (top, right), (bottom, left), (0, 0, 255), 2)
+        image_cropped = image[right:left, top:bottom]
+        image_transformed = test_transform(TF.to_pil_image(image_cropped)).unsqueeze(0)
+        age = model(image_transformed)
+        age = int(age.item())
+        label = f'age:{age}'
+        (w, h), _ = cv2.getTextSize(
+                label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+        cv2.putText(image, label, (bottom-w, right - h),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
-#   return preds, img
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.show()
