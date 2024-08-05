9c935e8329d9d99e9df3d77fdc21be03da4a49b4
Shaqayeq Matlabi <108318987+shgyg99@users.noreply.github.com>
2024-08-05 11:16:21 +0330

    added image uploader function in streamlit

--- .pyFiles/main.py
+++ .pyFiles/main.py
@@ -3,7 +3,11 @@ from PIL import Image
 import os
 import time
 import matplotlib.pyplot as plt
-uploaded = None
+from functions import Webcam, picture_upload, AgeEstimationModel
+from inputs import models
+from transforms import test_transform
+AgeEstimationModel()
+
 st.write("""
 ## Age Estimination App
 """)
@@ -11,14 +15,15 @@ st.write('---')
 
 st.sidebar.header('choose one of these options')
 
-pic = st.sidebar.button("upload a picture")
-camera = st.sidebar.button('take picture using webcam')
+pic = st.sidebar.button("upload a picture", type="secondary")
+camera = st.sidebar.button('take picture using webcam', type="secondary")
 
 # st.toast('hi')
 # st.balloons()
 # st.snow()
 up = st.file_uploader('upload', type=['png', 'jpg'])
-if up is not None:
+if up.read() is not None:
+    
     bar = st.progress(0)
     time.sleep(1)
     bar.progress(30)
@@ -27,8 +32,8 @@ if up is not None:
     time.sleep(1)
     bar.progress(100)
     st.image(up)
-    
+    file = up.read()
+    out = picture_upload(models['resnet50'], file, test_transform)
+    st.image(out)
 
-elif camera:
-    picture = st.camera_input('take a picture')
-
+pic = st.camera_input('take a picture')
